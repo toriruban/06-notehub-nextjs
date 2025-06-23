a2eb95cce7b5722ecb5c088c939fdba630255cf2
ToriRuban <toriruban@icloud.com>
2025-06-23 21:03:23 +0300

    feat: add components logic

---
+++ components/NoteList/NoteList.tsx
@@ -0,0 +1,42 @@
+import { Note } from '../../types/note';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { deleteNote } from '../../lib/api';
+import css from './NoteList.module.css';
+
+export interface NoteListProps {
+  notes: Note[];
+}
+
+export default function NoteList({ notes }: NoteListProps) {
+  const queryClient = useQueryClient();
+
+  const deleteMutation = useMutation({
+    mutationFn: deleteNote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['notes'] });
+    },
+  });
+
+  if (notes.length === 0) return null;
+
+  return (
+    <ul className={css.list}>
+      {notes.map(note => (
+        <li key={note.id} className={css.listItem}>
+          <h2 className={css.title}>{note.title}</h2>
+          <p className={css.content}>{note.content}</p>
+          <div className={css.footer}>
+            <span className={css.tag}>{note.tag}</span>
+            <button
+              className={css.button}
+              onClick={() => deleteMutation.mutate(note.id)}
+              disabled={deleteMutation.isPending}
+            >
+              {deleteMutation.isPending ? 'Deleting...' : 'Delete'}
+            </button>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+}
