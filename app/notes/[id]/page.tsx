b7e71b495c7d3798daf8eda9cd6eff41b829b83f
ToriRuban <toriruban@icloud.com>
2025-06-27 00:13:42 +0300

    update: name of the file

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,5 +1,5 @@
 import { fetchNoteById } from '@/lib/api';
-import NoteDetailsClient from './NoteDetailsClient'
+import NoteDetailsClient from './NoteDetails.client'
 import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
 
 type Props = {
